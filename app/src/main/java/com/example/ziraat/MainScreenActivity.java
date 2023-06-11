08f0b932b1e50869a09c201fc6f1f57c16d525f5
samiali12 <samijameel100@gmail.com>
2023-06-11 21:35:49 +0500

    remove unwanted files and resources

---
+++ app/src/main/java/com/example/ziraat/MainScreenActivity.java
@@ -0,0 +1,19 @@
+package com.example.ziraat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+
+    }
+
+}
--- app/src/main/java/com/example/ziraat/SplashScreenActivity.java
+++ app/src/main/java/com/example/ziraat/SplashScreenActivity.java
@@ -2,13 +2,29 @@ package com.example.ziraat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 3000; //splash screen will be shown for 2 seconds
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(SplashScreenActivity.this, MainScreenActivity.class));
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
     }
 }
